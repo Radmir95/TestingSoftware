7d55a81291da85d95a4fbf0a88f1b9b9a6c4ce05
Husnutdinov Radmir <XTreme95@yandex.ru>
2017-03-23 21:32:18 +0300

    Add class

---
+++ TestATM/ATM.WindowsForm/IntegrationTestForm1.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ATM.WindowsForm
+{
+    [TestClass()]
+    public class IntegrationTestForm1
+    {
+
+        [TestMethod()]
+        public void Form1_Form1ShouldBlockCreditCardAfter3IncorrectPin()
+        {
+            
+            var form = new Form1();
+            var t = typeof(Form1);
+
+            var atmFieldInfo = t.GetField("atm", BindingFlags.Instance | BindingFlags.NonPublic);
+            var atm = (TestATM.Core.ATM)atmFieldInfo.GetValue(form);
+
+            atm.CheckPinCode(1111);
+            atm.CheckPinCode(1111);
+            atm.CheckPinCode(1111);
+            atm.CheckPinCode(1111);
+
+            Assert.IsTrue(atm.IsCardBlock());
+
+        }
+
+    }
+}
