d0c5c0305e187940d6b60fb81ae1d265421fcef9
Husnutdinov Radmir <XTreme95@yandex.ru>
2017-03-23 21:32:51 +0300

    Revert "Revert "Add other things""
    This reverts commit 70346b724f4fcd14b73a73dc220853c37123d533.

--- TestATM/ATM.WindowsForm/Form1.Designer.cs
+++ TestATM/ATM.WindowsForm/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbPin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPin
+            // 
+            this.tbPin.Location = new System.Drawing.Point(120, 141);
+            this.tbPin.Name = "tbPin";
+            this.tbPin.Size = new System.Drawing.Size(100, 20);
+            this.tbPin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pin Code";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(131, 181);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(144, 223);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 294);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbPin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblError;
     }
 }
 
--- TestATM/ATM.WindowsForm/Form1.cs
+++ TestATM/ATM.WindowsForm/Form1.cs
@@ -1,20 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ATM.WindowsForm
 {
     public partial class Form1 : Form
     {
+
+        private TestATM.Core.ATM atm;
+
         public Form1()
         {
+
             InitializeComponent();
+
+            atm = new TestATM.Core.ATM(40000);
+            atm.InsertCard("43434");
+
         }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
+            var cardStatus = atm.IsCardBlock();
+
+            if (cardStatus == true)
+            {
+                lblError.Text = "Card is blocked";
+
+            }
+            else
+            {
+                var text = tbPin.Text;
+                var result = atm.CheckPinCode(Int32.Parse(text));
+
+                lblError.Text = result == true ? "Succeed" : "Error";
+
+            }
+
+        }
+
     }
 }
